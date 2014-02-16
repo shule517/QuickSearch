3c92ff9014f2140888878b5be8dfb508cd3d0596
シュール <shule517@gmail.com>
2014-02-16 15:20:25 +0900

    魔改造

--- QuickSearch/Form1.Designer.cs
+++ QuickSearch/Form1.Designer.cs
@@ -28,62 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(209, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 21);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "検索";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(0, 1);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 19);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
-            // 
-            // notifyIcon1
-            // 
-            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "QuickSearch\r\n";
-            this.notifyIcon1.Visible = true;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 22);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(300, 60);
-            this.MinimumSize = new System.Drawing.Size(300, 60);
-            this.Name = "Form1";
-            this.Text = "QuickSearch";
-            this.TopMost = true;
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.buttonSearch = new System.Windows.Forms.Button();
+			this.textBoxSearch = new System.Windows.Forms.TextBox();
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.webBrowser = new System.Windows.Forms.WebBrowser();
+			this.SuspendLayout();
+			// 
+			// buttonSearch
+			// 
+			this.buttonSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonSearch.Location = new System.Drawing.Point(411, 1);
+			this.buttonSearch.Name = "buttonSearch";
+			this.buttonSearch.Size = new System.Drawing.Size(75, 21);
+			this.buttonSearch.TabIndex = 0;
+			this.buttonSearch.Text = "検索";
+			this.buttonSearch.UseVisualStyleBackColor = true;
+			this.buttonSearch.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBoxSearch
+			// 
+			this.textBoxSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxSearch.Location = new System.Drawing.Point(0, 1);
+			this.textBoxSearch.Name = "textBoxSearch";
+			this.textBoxSearch.Size = new System.Drawing.Size(405, 19);
+			this.textBoxSearch.TabIndex = 1;
+			this.textBoxSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "QuickSearch\r\n";
+			this.notifyIcon1.Visible = true;
+			// 
+			// webBrowser
+			// 
+			this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.webBrowser.Location = new System.Drawing.Point(0, 21);
+			this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser.Name = "webBrowser";
+			this.webBrowser.ScriptErrorsSuppressed = true;
+			this.webBrowser.Size = new System.Drawing.Size(1046, 20);
+			this.webBrowser.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(490, 22);
+			this.Controls.Add(this.webBrowser);
+			this.Controls.Add(this.textBoxSearch);
+			this.Controls.Add(this.buttonSearch);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MinimumSize = new System.Drawing.Size(300, 60);
+			this.Name = "Form1";
+			this.Text = "QuickSearch";
+			this.TopMost = true;
+			this.Activated += new System.EventHandler(this.Form1_Activated);
+			this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBoxSearch;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.WebBrowser webBrowser;
 
     }
 }
--- QuickSearch/Form1.cs
+++ QuickSearch/Form1.cs
@@ -20,25 +20,78 @@ namespace QuickSearch
 
         private void button1_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start("http://ejje.weblio.jp/content/" + textBox1.Text);
+			SearchWord(textBoxSearch.Text);
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+		private void SearchWord(string word)
+		{
+			//webBrowser.Url = new Uri("http://eow.alc.co.jp/" + word + "#resultsList_contextmenu");
 
-        }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
+			webBrowser.Navigate("http://eow.alc.co.jp/" + word + "#resultsList_contextmenu");
+			SearchResultVisible(true);
+			webBrowser.DocumentCompleted += (sender, e) =>
+			{
+				try { webBrowser.Navigate("#resultsList_contextmenu"); }
+				catch { }
+			};
 
-        }
+			/*
+			webBrowser.Url = new Uri("http://eow.alc.co.jp/" + word + "#resultsList_contextmenu");
+			SearchResultVisible(true);
+			webBrowser.DocumentCompleted += (sender, e) =>
+			{
+				try { webBrowser.Navigate("#resultsList_contextmenu"); }
+				catch { }
+			};
+			 */
+		}
 
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == System.Windows.Forms.Keys.Enter)
             {
-                 System.Diagnostics.Process.Start("http://ejje.weblio.jp/content/" + textBox1.Text);
+				SearchWord(textBoxSearch.Text);
             }
+			else if (e.KeyCode == System.Windows.Forms.Keys.Escape)
+			{
+				SearchResultVisible(false);
+			}
         }
+
+		private void buttonToggle_Click(object sender, EventArgs e)
+		{
+			SearchResultVisible(!webBrowser.Visible);
+		}
+
+		private void SearchResultVisible(bool visible)
+		{
+			webBrowser.Visible = visible;
+
+			if (webBrowser.Visible)
+			{
+				ClientSize = new Size(ClientSize.Width, textBoxSearch.Height + 500);
+			}
+			else
+			{
+				ClientSize = new Size(ClientSize.Width, textBoxSearch.Height);
+			}
+		}
+
+		private void Form1_Activated(object sender, EventArgs e)
+		{
+			textBoxSearch.Focus();
+			SearchResultVisible(true);
+		}
+
+		private void Form1_Deactivate(object sender, EventArgs e)
+		{
+			SearchResultVisible(false);
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			ActiveControl = textBoxSearch;
+		}
     }
 }
